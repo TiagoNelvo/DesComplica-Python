ef3e5f0b1683654771b9fe4146d27da830f0a0eb
TiagoNelvo <ti.609@hotmail.com>
2024-03-15 17:41:51 -0300

    exemplo json

---
+++ funcionariosexemplojson.py
@@ -0,0 +1,15 @@
+import json
+
+with open('empregados.json', encoding='utf-8') as f:
+    dados = json.load(f)
+    
+
+for dado in dados['funcionarios']:
+    if dado['nome'] == 'Lucas': 
+        dado['endereco']['rua'] = 'Rua das Flores 40'
+        #print(dado['endereco']['rua'])
+        
+with open('empregados_modificado.json', 'w', encoding='utf-8') as f:
+    json.dump(dados, f, ensure_ascii=False, indent=4)
+        
+print(dados['funcionarios'][1]['endereco']['rua'])        
