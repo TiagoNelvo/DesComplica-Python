7b290a0ab4fcebf4ccf81eb54cc2dc0841b23000
TiagoNelvo <ti.609@hotmail.com>
2024-03-19 18:55:16 -0300

    pessoa Fisica

--- Orientação a Objetos/pessoafisica.py
+++ Orientação a Objetos/pessoafisica.py
@@ -1,1 +1,20 @@
-class
+from pessoa import Pessoa
+
+
+class PessoaFisica(pessoa):
+    
+    def __init__(self, CPF, nome, idade):
+        super().__init__(nome,idade)
+        self.cpf = CPF
+        
+def getCPF(self):
+    return self.cpf
+
+def setCPF(self, CPF):
+    self.cpf = CPF
+    
+    
+pessoaFisica = PessoaFisica("22244455599", "Marcelo", 33)
+print(pessoaFisica.getNome())
+print(pessoaFisica.getIdade())
+print(pessoaFisica.getCPF())
